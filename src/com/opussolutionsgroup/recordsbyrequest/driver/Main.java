8d921fea2c77dd4ffd16c977adb515254b87d3e1
jsap1075 <jsap1075@10.1.138.184>
2017-03-14 16:53:23 -0500

    config

--- src/com/opussolutionsgroup/recordsbyrequest/driver/Main.java
+++ src/com/opussolutionsgroup/recordsbyrequest/driver/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		assignProjectConfig();
-		assignColumnConfig();
+	//	assignColumnConfig();
 	}
 
 	private static void assignProjectConfig() {
